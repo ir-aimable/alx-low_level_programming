e55e4173e79be1aaaa708669c60f4c2fa07fd39e
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-12-09 05:36:16 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/** 
-* this is a line
-*and this is a documentation
-*/
-
-/* Return: Always 0 (Success) */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
