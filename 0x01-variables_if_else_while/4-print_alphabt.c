fee72ec1723584552f7241eac315eb89a9bb2a22
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-12-10 05:29:48 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 char low, e, q;
@@ -18,6 +16,5 @@ if (low != e && low != q)
 putchar(low);
 }
 putchar('\n');
-
 return (0);
 }
