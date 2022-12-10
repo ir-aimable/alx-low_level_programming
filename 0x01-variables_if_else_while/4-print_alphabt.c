68c166fe6f3cdc2087f4970e50fd8568395de2bb
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-12-10 05:13:41 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-	char low, e, q;
+char low, e, q;
 
-	e = 'e';
-	q = 'q';
+e = 'e';
+q = 'q';
 
-	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
