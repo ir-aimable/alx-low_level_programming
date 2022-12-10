82603263880c9bea0d20cd8081120968a4dee070
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-12-10 05:25:06 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,17 +4,13 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
-
-	return (0);
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
 }
