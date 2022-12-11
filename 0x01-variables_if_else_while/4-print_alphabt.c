c4ea71b3738ccafe6707976359e30830aacf50df
Aimable Iradukunda <aimableelleven@gmail.com>
2022-12-10 21:37:46 -0800

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
- * Return: Always 0 (Success)
- */
+ * return 0
+*/
 int main(void)
 {
-char low, e, q;
-e = 'e';
-q = 'q';
-for (low = 'a'; low <= 'z'; low++)
-{
-if (low != e && low != q)
-putchar(low);
+	char low;
+	char e='e';
+	char q='q';
+
+	for (low='a'; low<='z'; low++)
+	{
+	if (low !=e &&low !=q)
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
 }
-putchar('\n');
-return (0);
-}
+
