207294ee056a6c0bb364a4855de82a0b8972bea2
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-12-14 03:14:39 +0200

    battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  *  * print_to_98 - Check Holberton
@@ -10,28 +10,28 @@
  *       */
 void print_to_98(int n)
 {
-		if (n <= 98)
-				{
-							for (; n <= 98; n++)
-										{
-														printf("%d", n);
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+printf("%d", n);
 
-																	if (n == 98)
-																						continue;
-																				printf(", ");
-																						}
-									printf("\n");
-										}
-			else
-					{
-								for (; n >= 98; n--)
-											{
-															printf("%d", n);
+if (n == 98)
+continue;
+printf(", ");
+}
+printf("\n");
+}
+else
+{
+for (; n >= 98; n--)
+{
+printf("%d", n);
 
-																		if (n == 98)
-																							continue;
-																					printf(", ");
-																							}
-										printf("\n");
-											}
+if (n == 98)
+continue;
+printf(", ");
+}
+printf("\n");
+}
 }
