34a08744f372011f3159af992914d7e6e7f3c4bd
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-12-14 03:15:42 +0200

    battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- *  * print_to_98 - Check Holberton
- *   * @n: A input integer
- *    * Description: function that prints all natural numbers
- *     * from n to 98, followed by a new line.
- *      * Return: Nothing
- *       */
+ * print_to_98 - Check the code
+ * @n: A input integer
+ * Description: function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ * Return: Nothing
+ */
 void print_to_98(int n)
 {
 if (n <= 98)
